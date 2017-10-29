808d3852fb64c72bb6e1c5c7ae5506762c8f71d6
schao0531 <staceychao2019@u.northwestern.edu>
2017-10-29 14:28:53 -0500

    started working on audio

--- Gigbot/src/pages/home/home.ts
+++ Gigbot/src/pages/home/home.ts
@@ -1,14 +1,68 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { MediaPlugin } from 'ionic-native';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+@Injectable()
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  media: MediaPlugin;
 
+  ionViewLoaded() {
+    this.media = new MediaPlugin('../Library/NoCloud/recording.wav')
   }
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+  }
+
+startRecording() {
+  try {
+    this.media.startRecord();
+  }
+  catch (e) {
+    this.showAlert((<Error>e).message);
+  }
+}
+
+stopRecording() {
+  try {
+    this.media.stopRecord();
+  }
+  catch (e) {
+    this.showAlert((<Error>e).message)
+  }
+}
+
+startPlayback() {
+  try {
+    this.media.play();
+  }
+  catch (e) {
+    this.showAlert((<Error>e).message);
+  }
+}
+
+stopPlayback() {
+  try {
+    this.media.stop();
+  }
+  catch (e) {
+    this.showAlert((<Error>e).message);
+  }
+}
+
+showAlert(message) {
+  let alert = this.alertCtrl.create({
+    title: 'Error',
+    subTitle: message,
+    buttons: ['OK']
+  });
+  alert.present();
+}
 
 }
