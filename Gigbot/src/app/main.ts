77b54411afb5cd741b67064358f4080bfc824b08
Ivan Carrasco <ivancarrascoq@gmail.com>
2017-11-02 12:37:09 -0300

    layout 3 levels completed

--- Gigbot/src/app/main.ts
+++ Gigbot/src/app/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 //
 import { NavController } from 'ionic-angular';
 //import { MainPage } from '../main/main';
-import { ChatPage } from '../pages/chat/chat';
-import { LibraryPage } from '../pages/library/library';
-import { PracticePage } from '../pages/practice/practice';
-import { NetworkPage } from '../pages/network/network';
+
 //
 platformBrowserDynamic().bootstrapModule(AppModule);
 
@@ -17,16 +14,5 @@ export class MainPage {
   
     constructor(public navCtrl: NavController) {
     }
-    navchatpage(){
-      this.navCtrl.push(ChatPage);
-    }
-    navlibrarypage(){
-        this.navCtrl.push(LibraryPage);
-    }
-    navpracticepage(){
-        this.navCtrl.push(PracticePage);
-    }  
-    navnetworkpage(){
-        this.navCtrl.push(NetworkPage);
-    }
+
   }
--- Gigbot/src/pages/main/main.ts
+++ Gigbot/src/pages/main/main.ts
@@ -7,6 +7,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+import { ChatPage } from '../chat/chat';
+import { LibraryPage } from '../library/library';
+import { PracticePage } from '../practice/practice';
+import { NetworkPage } from '../network/network';
+
+//
 
 @IonicPage()
 @Component({
@@ -21,5 +27,16 @@ export class MainPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad MainPage');
   }
-
+  navchatpage(){
+    this.navCtrl.push(ChatPage);
+  }
+  navlibrarypage(){
+      this.navCtrl.push(LibraryPage);
+  }
+  navpracticepage(){
+      this.navCtrl.push(PracticePage);
+  }  
+  navnetworkpage(){
+      this.navCtrl.push(NetworkPage);
+  }
 }
