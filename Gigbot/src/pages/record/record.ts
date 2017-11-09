5fdaafc36aee47f6f3d2a207fc4b9a000f83d03a
schao0531 <staceychao2019@u.northwestern.edu>
2017-11-09 15:27:58 -0600

    add record page functionality

--- Gigbot/src/pages/record/record.ts
+++ Gigbot/src/pages/record/record.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild} from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController, Platform, ViewController } from 'ionic-angular';
+import { MediaPlugin, MediaCapture, Camera} from 'ionic-native';
+import { Media, MediaObject} from '@ionic-native/media';
+import { File } from '@ionic-native/file';
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { CountdownPage } from '../countdown/countdown';
+
+import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview';
 
 /**
  * Generated class for the RecordPage page.
@@ -13,13 +22,129 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-record',
   templateUrl: 'record.html',
 })
+
+@Injectable()
 export class RecordPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  mediaPlugin: MediaPlugin = null;
+  recorded: boolean;
+  questions: Observable<any[]>;
+  questionsArray: any = [];
+  questionIndex: number = 0;
+
+  state : String;
+  //state : AudioRecorderState = AudioRecorderState.Ready;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  public alertCtrl: AlertController,
+  public platform: Platform, db:AngularFireDatabase,
+  private cameraPreview: CameraPreview) {
+
+      this.recorded = false;
+      this.state = 'ready';
+      this.questions = db.list("/Questions").valueChanges();
+      this.questions.subscribe(questions => {
+        this.questionsArray = questions;
+        console.log(this.questionsArray);});
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RecordPage');
+  options = {
+    x: 0,
+    y: 0,
+    width: window.screen.width,
+    height: window.screen.height,
+    camera: this.cameraPreview.CAMERA_DIRECTION.BACK,
+    toBack: false,
+    tapPhoto: true,
+    tapFocus: false,
+    previewDrag: false
+  };
+
+  get MediaPlugin(): MediaPlugin {
+  if (this.mediaPlugin == null) {
+    this.mediaPlugin = new MediaPlugin('recording.wav');
+  }
+  return this.mediaPlugin;
+}
+
+ionViewDidLoad() {
+  console.log('ionViewDidLoad PracticePage');
+}
+
+startVideoRecording(){
+  console.log('StartVideoRecording');
+  this.cameraPreview.startCamera(this.options).then(
+  (res) => {
+    console.log(res)
+  },
+  (err) => {
+    console.log(err)
+  });;
+
+}
+
+
+  startAudioRecording() {
+    try {
+        this.MediaPlugin.startRecord();
+        //this.state = AudioRecorderState.Recording;
+        this.state = 'recording';
+        console.log("success startRecording");
+    }
+    catch (e) {
+      this.showAlert((<Error>e).message);
+    }
+  }
+
+  stopRecording() {
+    try {
+      this.MediaPlugin.stopRecord();
+      //this.state = AudioRecorderState.Recorded;
+      this.state = 'recorded';
+      this.recorded = true;
+      console.log("success stopRecording");
+    }
+    catch (e) {
+      this.showAlert((<Error>e).message)
+    }
+  }
+
+  newQuestion() {
+    this.questionIndex = (this.questionIndex + 1) % this.questionsArray.length;
+  }
+
+  playRecording() {
+    try {
+      this.MediaPlugin.play();
+      //this.state = AudioRecorderState.Playing;
+      this.state = 'playing';
+      console.log("success playRecording");
+    }
+    catch (e) {
+      this.showAlert((<Error>e).message);
+    }
+  }
+
+  stopRecordingPlay(){
+    try {
+      this.MediaPlugin.stop();
+      //this.state = AudioRecorderState.Ready;
+      this.state = 'ready';
+      console.log("success stopRecordingPlay");
+    }
+    catch (e) {
+      this.showAlert((<Error>e).message);
+    }
+  }
+
+  showAlert(message) {
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
